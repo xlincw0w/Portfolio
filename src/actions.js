06e5a89af0029bcd136221941ca8c81d10a559e8
xlincw0w <khaled.khazem@yahoo.fr>
2019-03-23 21:40:35 +0100

    Implementing redux

---
+++ src/actions.js
@@ -0,0 +1,6 @@
+export const NAVIGATION = 'NAVIGATION'
+
+export const Navigate = (way) => ({
+    type: NAVIGATION,
+    render: way
+  })
---
+++ src/reducers.js
@@ -0,0 +1,23 @@
+import { NAVIGATION } from './actions';
+import { combineReducers } from 'redux'
+
+const initialState = {
+    render: 'home'
+  }
+
+function FirstReducer(state = initialState, action) {
+    switch (action.type) {
+      case NAVIGATION:
+        return Object.assign({}, state, {
+            render: action.render
+        })
+      default:
+        return state
+    }
+  }
+
+  const rootReducer = combineReducers({
+    FirstReducer
+  })
+  
+  export default rootReducer;
