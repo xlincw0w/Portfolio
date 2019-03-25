a8c34d3324b409a13894837f5fa1fd45d8b401a8
xlincw0w <khaled.khazem@yahoo.fr>
2019-03-25 14:54:11 +0100

    build

---
+++ build/precache-manifest.145c4fcb90b522e877b2d28e1fa154c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "350b7f7fb70983da33cdccabc4dcd5df",
+    "url": "/static/media/spider.350b7f7f.png"
+  },
+  {
+    "revision": "92ff4648630df626aec647fb5004b7c0",
+    "url": "/static/media/khaled.92ff4648.jpg"
+  },
+  {
+    "revision": "201103f8cc8289872eebfc479c91e74f",
+    "url": "/static/media/cloud.201103f8.jpg"
+  },
+  {
+    "revision": "fbab70127936af33dc56b13d42054331",
+    "url": "/static/media/bg1.fbab7012.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0adf117b6e253ceaae92",
+    "url": "/static/js/main.578c7d88.chunk.js"
+  },
+  {
+    "revision": "39b370920622e45040ef",
+    "url": "/static/js/2.d09984e1.chunk.js"
+  },
+  {
+    "revision": "0adf117b6e253ceaae92",
+    "url": "/static/css/main.27cf30d4.chunk.css"
+  },
+  {
+    "revision": "39b370920622e45040ef",
+    "url": "/static/css/2.7c66527e.chunk.css"
+  },
+  {
+    "revision": "98fb5fc29603ef56147feaefca37da00",
+    "url": "/index.html"
+  }
+];
